8e52f7874bbb6e658ae2cf5cb8c897dd0aaee264
kramerkat <kevzhuart@gmail.com>
2025-02-23 00:32:11 -0500

    improvements

--- docs/d3js/sankey.js
+++ docs/d3js/sankey.js
@@ -14,15 +14,15 @@ function createSankey(data) {
 
   // Set up the Sankey diagram layout
   const sankey = d3.sankey()
-    .nodeWidth(50)
-    .nodePadding(40)
-    .extent([[50, 1], [width - 50, height - 2]])
-    .nodeSort((a, b) => a.name.localeCompare(b.name));  // Sort nodes to minimize crossings
+    .nodeWidth(20)
+    .nodePadding(10)
+    .extent([[10, 1], [width - 200, height - 2]])
+    .nodeSort((a, b) => a.displayName.localeCompare(b.displayName));  // Sort nodes to minimize crossings
 
   // Create a map to convert node names to indices
   const nodeMap = {};
   data.nodes.forEach((node, i) => {
-    nodeMap[node.name] = i;
+    nodeMap[node.id] = i;
   });
 
   // Convert link source and target names to indices
@@ -32,10 +32,9 @@ function createSankey(data) {
       source: nodeMap[d.source],
       target: nodeMap[d.target],
       value: d.value || 0,  // Default to 0 if value is not provided
-      linkfill: d.linkfill,
-      linkstroke: d.linkstroke,
-      rectfill: d.rectfill,
-      rectstroke: d.rectstroke
+      linkFill: d.linkFill,
+      linkStroke: d.linkStroke,
+      linkOpacity: d.linkOpacity
     }))
   });
 
@@ -47,21 +46,21 @@ function createSankey(data) {
     .attr("class", "link")
     .attr("d", d3.sankeyLinkHorizontal())
     .style("stroke-width", d => Math.max(1, d.width))
-    .style("fill", d => d.linkfill || "none")
-    .style("stroke", d => d.linkstroke || "#000")
-    .style("stroke-opacity", 0.2);
+    .style("fill", d => d.linkFill || "none")
+    .style("stroke", d => d.linkStroke || "#000")
+    .style("stroke-opacity", d => d.linkOpacity || 0.4);
 
   // Add link values
-  svg.append("g")
-    .selectAll(".link-value")
-    .data(graph.links)
-    .enter().append("text")
-    .attr("class", "link-value")
-    .attr("x", d => (d.source.x1 + d.target.x0) / 2)
-    .attr("y", d => (d.source.y1 + d.target.y0) / 2)
-    .attr("dy", ".35em")
-    .style("text-anchor", "middle")
-    .text(d => `$${d.value.toLocaleString()}`);
+  // svg.append("g")
+  //   .selectAll(".link-value")
+  //   .data(graph.links)
+  //   .enter().append("text")
+  //   .attr("class", "link-value")
+  //   .attr("x", d => (d.source.x1 + d.target.x0) / 2)
+  //   .attr("y", d => (d.source.y1 + d.target.y0) / 2)
+  //   .attr("dy", ".35em")
+  //   .style("text-anchor", "middle")
+  //   .text(d => `$${d.value.toLocaleString()}`);
 
   // Add nodes (elements)
   const node = svg.append("g")
@@ -71,33 +70,30 @@ function createSankey(data) {
     .attr("class", "node")
     .attr("transform", d => `translate(${d.x0},${d.y0})`);
 
+  // Add node rectangles
   node.append("rect")
     .attr("height", d => d.y1 - d.y0)
-    .attr("width", sankey.nodeWidth())
+    .attr("width", d => sankey.nodeWidth())
     .style("fill", d => d.rectfill || "#ccc")
     .style("stroke", d => d.rectstroke || "#000")
     .style("stroke-width", 1);
 
+  // Add node names
+  const textOffset = sankey.nodeWidth() + 10
   node.append("text")
-    .attr("x", 5)  // Add some padding to the left
+    .attr("x", textOffset)  // Add some padding to the left
     .attr("y", d => (d.y1 - d.y0) / 2)
     .attr("dy", ".35em")
-    .style("text-anchor", "middle")
-    .text(d => d.name)
-    .each(function (d) {
-      const textWidth = this.getBBox().width;
-      d3.select(this.parentNode).select("rect")
-        .attr("width", textWidth + 10)  // Set width of node based on text
-        .attr("x", -textWidth / 2);  // But keep it centered
-    });
+    .style("text-anchor", "start")
+    .text(d => d.displayName)
 
   // Add node values
   node.append("text")
-    .attr("x", 5)  // Add some padding to the left
+    .attr("x", textOffset)  // Add some padding to the left
     .attr("y", d => (d.y1 - d.y0) / 2 + 20)  // Position below the node name
     .attr("dy", ".35em")
-    .style("text-anchor", "middle")
-    .text(d => d.value ? d.value.toLocaleString() : '');
+    .style("text-anchor", "start")
+    .text(d => d.value ? `$${d.value.toLocaleString()}` : '');
 }
 
 // Function to update the Sankey diagram with custom values
@@ -112,23 +108,33 @@ function updateSankey() {
   d3.json("sankey.json").then(data => {
     // Update the paycheck-related values
     data.links.forEach(link => {
-      if (link.source === "401k Employer Match") {
+      // Intake settings
+      if (link.source === "401k-contributions-employer") {
         link.value = employerMatch;
       }
-      if (link.source === "Paycheck") {
-        if (link.target === "401k Pre-Tax Contributions") {
+      if (link.source === "paycheck") {
+        if (link.target === "401k-contributions-pretax") {
           link.value = preTaxContribution;
-        } else if (link.target === "Taxes") {
+        } else if (link.target === "taxes") {
           link.value = taxes;
-        } else if (link.target === "Post-Tax Pay") {
+        } else if (link.target === "paycheck-posttax") {
           link.value = postTaxPay;
         }
       }
-      if (link.source === "Post-Tax Pay") {
-        if (link.target === "401k Post-Tax Contributions") {
+      // Calculate remaining paycheck
+      if (link.source === "paycheck-posttax") {
+        if (link.target === "401k-contributions-posttax") {
           link.value = postTaxContribution;
-        } else if (link.target === "Checking Account") {
+        } else if (link.target === "checking") {
           link.value = postTaxPay - postTaxContribution;
+        }
+      }
+      // Calculate total 401k contributions
+      if (link.target === "401k-contributions") {
+        if (link.source === "401k-contributions-pretax") {
+          link.value = preTaxContribution;
+        } else if (link.source === "401k-contributions-posttax") {
+          link.value = postTaxContribution;
         }
       }
     });
